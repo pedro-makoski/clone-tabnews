d1768e9a3ccd74a3bad103b9e7a5906e1c520080
Pedro Makoski <makoski.pedrogamail.com@gmail.com>
2026-02-24 17:47:48 -0300

    fix: type error

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
     return <>
-        <h1><q>Obrigado por ser uma mãe incrivel, você é muito especial❤️!</q><br></br>- de Pedro Makoski para Tatiana M. Makoski</h1>
+        <h1><q>Obrigado por ser uma mãe incrível, você é muito especial❤️!</q><br></br>- de Pedro Makoski para Tatiana M. Makoski</h1>
         <div className="hearts">
             <span>❤️</span>
             <span>❤️</span>
